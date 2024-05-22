9124f66075708a27f51fd611004438f1511fd7f3
manidixit51 <manidixit512@gmail.com>
2024-05-22 17:46:44 +0530

    modified toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,26 +2,38 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
+  SiAdobeillustrator,
+  SiAdobephotoshop,
   SiVercel,
-  SiMacos,
+  SiFigma,
+  SiGithub,
+  SiIntellijidea,
+
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiFigma/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithub/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiIntellijidea/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
+        <SiAdobeillustrator />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
+        <SiAdobephotoshop />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
