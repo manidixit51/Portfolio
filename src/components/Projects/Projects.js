39039bd359f1dda71c6b26e8d4f596b3a770ebe0
manidixit51 <manidixit512@gmail.com>
2024-05-23 19:03:47 +0530

    modified projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ function Projects() {
               isBlog={false}
               title="Crypto Hunter"
               description="Crypto Hunter is a sleek and intuitive application designed to empower cryptocurrency enthusiasts with real-time market insights. Integrated with Coin Gecko API for live updates, and fortified with Firebase for secure authentication and data storage, users can navigate the ever-changing landscape of digital currencies effortlessly. Built with React.js and Material UI, its responsive design ensures seamless access across devices, providing users with a visually appealing and dynamic tool for informed decision-making in the world of cryptocurrencies."
-              ghLink="https://github.com/nimitagarwal26/CryptoHunter"
+              ghLink="https://github.com/manidixit51/CryptoHunter"
               
             />
           </Col>
@@ -38,7 +38,7 @@ function Projects() {
               isBlog={false}
               title="Static Code Analyser"
               description="The Static Code Analyzer is a robust tool designed to analyze Java source code statically, identifying potential issues, violations of coding conventions, and design flaws. It dynamically processes XML-based rules for flexible rule definitions and customization. The tool integrates a perceptron-based AI model to evaluate code quality, detect patterns, and identify areas for improvement. Additionally, it features a comment density checker to ensure the maintainability and clarity of the codebase by evaluating the adequacy of code comments."
-              ghLink="https://github.com/K144U/--static_code_analysis_ISRO_respondbasket"
+              ghLink="https://github.com/manidixit51/Static-Code-Analyser"
             />
           </Col>
 
